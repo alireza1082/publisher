de8e3d7116c53f33fbdbda2be85d39cd694a902d
alireza <rezaali1387@gmail.com>
2023-11-30 17:58:08 +0330

    Prepare plugin requirements

--- publisher/src/main/java/ir/sharif/publisher/PublishTask.kt
+++ publisher/src/main/java/ir/sharif/publisher/PublishTask.kt
@@ -1,5 +1,10 @@
 package ir.sharif.publisher
 
-class PublishTask {
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+class PublishTask() : Plugin<Project> {
+    override fun apply(target: Project) {
+        TODO("Not yet implemented")
+    }
 
 }
